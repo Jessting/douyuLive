de8050e3d9ca7afbdd6d6e48922a5a6e7b04421a
jess <1094372441@qq.com>
2019-05-10 14:19:47 +0800

    初始化信息

---
+++ douyuLive/douyuLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  douyuLive
+//
+//  Created by FDC on 2019/5/10.
+//  Copyright © 2019 FDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
